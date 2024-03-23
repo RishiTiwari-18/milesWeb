310fcae6913ef90b5c756ae9ee1ceb56f4e25ae8
RishiTiwari-18 <153971668+RishiTiwari-18@users.noreply.github.com>
2024-03-23 10:44:33 +0000

    nav

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//alert("!!!!");
+let navContent = document.querySelector(".nav-content")
+let navIcon = document.querySelector("#nav-button i")
+let navButton = document.querySelector("#nav-button")
+let flag = 0;
+
+navButton.addEventListener("click", function(){
+
+    if (flag == 0) {
+        navContent.style.right = "17%";
+        navIcon.style.rotate = "-90deg";
+        flag = 1;
+    } else {
+        navContent.style.right = "-50%";
+        navIcon.style.rotate = "0deg";
+        flag = 0;
+    }
+
+})
