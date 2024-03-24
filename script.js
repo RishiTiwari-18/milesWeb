1eafd60daf3a9f87f129034cb2b5884c22fcefcd
RishiTiwari-18 <153971668+RishiTiwari-18@users.noreply.github.com>
2024-03-24 10:33:15 +0000

    great work!

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ let vps = document.querySelector(".vps-page");
 let vpsBtn = document.querySelector(".vps");
 let about = document.querySelector(".about-page");
 let aboutBtn = document.querySelector(".about");
-let flag = 1;
-let flag2 = 1;
-let flag3 = 1;
 
 const navButtons = [hostingBtn, vpsBtn, aboutBtn];
 const navCards = [hosting, vps, about];
@@ -28,7 +25,6 @@ function openCard() {
                 gsap.to(navCards[i], {
                     top: '-100%'
                 })
-                console.log(i);
                 flags[i] = 1;
             }
         }
@@ -43,48 +39,7 @@ hostingBtn.addEventListener("click", openCard);
 vpsBtn.addEventListener("click", openCard);
 aboutBtn.addEventListener("click", openCard);
 
-// hostingBtn.addEventListener("click", function () {
-//     if (flag == 1) {
-//         gsap.to(hosting, {
-//             top: '0%',
-//         })
-//         flag = 0;
-//     } else {
-//         gsap.to(hosting, {
-//             top: '-100%'
-//         })
-//         flag = 1;
-//     }
-// })
 
-
-// vpsBtn.addEventListener("click", function () {
-//     if (flag2 == 1) {
-//         gsap.to(vps, {
-//             top: '0%',
-//         })
-//         flag2 = 0;
-//     } else {
-//         gsap.to(vps, {
-//             top: '-100%'
-//         })
-//         flag2 = 1;
-//     }
-// })
-
-// aboutBtn.addEventListener("click", function () {
-//     if (flag3 == 1) {
-//         gsap.to(about, {
-//             top: '0%',
-//         })
-//         flag3 = 0;
-//     } else {
-//         gsap.to(about, {
-//             top: '-100%'
-//         })
-//         flag3 = 1;
-//     }
-// })
 
 
 
